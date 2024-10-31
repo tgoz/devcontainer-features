7e09581ccb1f861dcc82e5a9b121291850fa7fe7
Tony Galliford <tony@galliford.org>
2024-10-31 16:46:47 +1100

    Create Supabase feature

---
+++ src/supabase/README.md
@@ -0,0 +1,11 @@
+# Supabase devcontainer feature
+
+Install Supabase for use with Docker-in-Docker (via docker compose)
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/tgoz/devcontainer-features/supabase:0.0.1": {}
+}
+```
---
+++ src/supabase/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -euo pipefail
+
+echo "===================================================="
+echo "STARTING INSTALL.SH FROM SUPABASE FEATURE"
+echo "===================================================="
+
+set -x
+id
+pwd
+set +x
+
+git clone --filter=blob:none --no-checkout https://github.com/supabase/supabase
+cd supabase
+git sparse-checkout set --cone docker && git checkout master
+
+# Go to the docker folder
+cd docker
+
+# Copy the fake env vars
+cp .env.example .env
+
+# Pull the latest images
+docker compose pull
+
+echo "===================================================="
