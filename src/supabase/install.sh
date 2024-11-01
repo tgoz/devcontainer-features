9c439835ecad596c4fc62bddb81d219a028f3bf5
Tony Galliford <tony@galliford.org>
2024-11-01 14:44:19 +1100

    Clean up install script

--- src/supabase/install.sh
+++ src/supabase/install.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 
-set -ex
+set -e
 
-echo "===================================================="
-echo "STARTING INSTALL.SH FROM SUPABASE FEATURE"
-echo "===================================================="
-
-date
-id
-pwd
-
-cp supabase /usr/local/bin/supabase
-chmod +x /usr/local/bin/supabase
+FEATURE_DIR="$(pwd)"
 
 cd /opt
 
 git clone --filter=blob:none --no-checkout https://github.com/supabase/supabase
 cd supabase
 git sparse-checkout set --cone docker && git checkout master
+rm -rf .git
 
-echo "===================================================="
+cp "${FEATURE_DIR}/supabase" /usr/local/bin/supabase
+chmod +x /usr/local/bin/supabase
