a7dab4403b8d6a22a3e001eaab54635efee00da1
Tony Galliford <tony@galliford.org>
2024-11-01 14:11:20 +1100

    Download supabase to /opt

--- src/supabase/install.sh
+++ src/supabase/install.sh
@@ -1,51 +1,20 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 echo "===================================================="
 echo "STARTING INSTALL.SH FROM SUPABASE FEATURE"
 echo "===================================================="
 
-set -x
 date
 id
-pwd
-set +x
+cd /opt
 
 git clone --filter=blob:none --no-checkout https://github.com/supabase/supabase
 cd supabase
 git sparse-checkout set --cone docker && git checkout master
 
-# Go to the docker folder
-cd docker
-
-# Copy the fake env vars
-cp .env.example .env
-
-cat >/usr/local/bin/supabase <<EOF
-#!/bin/sh
-
-case "$1" in
-    pull)
-        docker compose pull
-    ;;
-    up)
-        docker compose up -d
-    ;;
-    stop)
-        docker compose down
-    ;;
-    remove)
-        docker compose down -v --remove-orphans
-    ;;
-    *)
-        :
-esac
-
-EOF
+cp supabase /usr/local/bin/supabase
 chmod +x /usr/local/bin/supabase
-
-pwd >/install-pwd
-ls -lR >/install-ls-lR
 
 echo "===================================================="
